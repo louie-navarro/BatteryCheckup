936f369ca7a4a378fe112ddd0f7bb2cffc5f9c75
Louie Navarro <louie.navarro@asurion.com>
2024-02-23 22:51:54 +0800

    add missing name

--- packages/expo-battery-plus/ios/ExpoBatteryPlusModule.swift
+++ packages/expo-battery-plus/ios/ExpoBatteryPlusModule.swift
@@ -2,6 +2,8 @@ import ExpoModulesCore
 
 public class ExpoBatteryPlusModule: Module {
   public func definition() -> ModuleDefinition {
+    Name("ExpoBatteryPlus")
+
     AsyncFunction("getBatteryData") { 
       return nil;
     }
