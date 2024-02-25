83552ef144d3bbd912a6c55e12616cf682940fa8
Louie Navarro <louie.navarro@asurion.com>
2024-02-25 21:19:14 +0800

    add other android battery details

--- packages/battery-plus/android/src/main/java/expo/modules/batteryplus/BatteryPlusModule.kt
+++ packages/battery-plus/android/src/main/java/expo/modules/batteryplus/BatteryPlusModule.kt
@@ -1,5 +1,6 @@
 package expo.modules.batteryplus
 
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
@@ -17,19 +18,54 @@ class BatteryPlusModule : Module() {
         context.registerReceiver(null, filter)
       }
 
-      val health = intent?.getIntExtra(BatteryManager.EXTRA_HEALTH, -1) ?: -1
-      val healthText =  when(health) {
-        BatteryManager.BATTERY_HEALTH_GOOD -> "GOOD"
-        BatteryManager.BATTERY_HEALTH_COLD -> "COLD"
-        BatteryManager.BATTERY_HEALTH_OVERHEAT -> "OVER-HEAT"
-        BatteryManager.BATTERY_HEALTH_DEAD -> "DEAD"
-        BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE -> "OVER-VOLTAGE"
-        BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE -> "FAILED"
-        else -> "UNKNOWN"
-      }
+      val rawHealth = intent?.getIntExtra(BatteryManager.EXTRA_HEALTH, -1) ?: -1
+      val health = getHealthText(rawHealth)
 
-      return@Function mapOf("health" to healthText)
+      val temperature = intent?.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1) ?: -1
+
+      val rawVoltage = intent?.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1) ?: -1
+      val voltage = getBatteryVoltage(rawVoltage)
+
+      val technology = intent?.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY)
+
+      val cycleCount = intent?.getIntExtra(BatteryManager.EXTRA_CYCLE_COUNT, -1 )?: -1
+
+      val batteryManager = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+      val chargeContext =  batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER)
+      val capacity = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+      val maxCapacity = getMaxCapacity(chargeContext, capacity)
+
+
+      return@Function mapOf(
+              "health" to health,
+              "temperature" to temperature,
+              "voltage" to voltage,
+              "technology" to technology,
+              "cycleCount" to cycleCount,
+              "maxCapacity" to maxCapacity,
+      )
     }
+  }
 
+  private fun getBatteryVoltage(voltage: Int): Float {
+    return if (voltage > 1000) voltage / 1000f else voltage.toFloat()
+  }
+
+  private fun getHealthText(health: Int): String {
+    return when(health) {
+      BatteryManager.BATTERY_HEALTH_GOOD -> "GOOD"
+      BatteryManager.BATTERY_HEALTH_COLD -> "COLD"
+      BatteryManager.BATTERY_HEALTH_OVERHEAT -> "OVER-HEAT"
+      BatteryManager.BATTERY_HEALTH_DEAD -> "DEAD"
+      BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE -> "OVER-VOLTAGE"
+      BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE -> "FAILED"
+      else -> "UNKNOWN"
+    }
+  }
+
+  private fun getMaxCapacity(chargeCounter: Int, capacity: Int): Float {
+    if(chargeCounter == Integer.MIN_VALUE || capacity == Integer.MIN_VALUE)
+      return 0f
+    return  ((chargeCounter / capacity) * 100).toFloat()
   }
 }
